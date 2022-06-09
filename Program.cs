d8a4e7a97145c69c973f278c139b919acfa46bb2
Natanael Vieira <natanaelvieira130@gmail.com>
2022-06-09 10:52:41 -0300

    M2S02 - Exercicio 6

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace GeraEstoque {
     class Program {
         static void Main(string[] args)
         {
-            
+            System.Console.WriteLine("Seja Bem Vindo ao GeraEstoque 1.0");
+            System.Console.WriteLine("------------------------------------------");
+
+            System.Console.WriteLine("1 - Cadastrar Produto");
+            System.Console.WriteLine("2 - Consultar Produto");
+            System.Console.WriteLine("3 - Modificar Produto");
+            System.Console.WriteLine("4 - Excluir Produto");
+            System.Console.WriteLine("0 - Sair");
+
+            System.Console.WriteLine("Digite a opção:");
+            Console.ReadLine();
         }
     }
 }
