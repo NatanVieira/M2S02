ef25942762860e6a1f71322a4390077f6f4686e2
Natanael Vieira <natanaelvieira130@gmail.com>
2022-06-09 11:13:00 -0300

    M2S02 - Exercicio8

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
-
+using System;
 namespace GeraEstoque {
 
     class Program {
         static void Main(string[] args)
         {
+            string? nome = "";
+            double? qtdEstoque = 0;
+            double? valorCompra = 0;
+            double? valorVenda = 0;
+
             System.Console.WriteLine("Seja Bem Vindo ao GeraEstoque 1.0");
             System.Console.WriteLine("------------------------------------------");
 
@@ -27,7 +32,15 @@ namespace GeraEstoque {
             }
 
             void CadastrarProduto(){
-                System.Console.WriteLine("Cadastra Produto");    
+                Console.WriteLine("Preencha as informações abaixo");
+                Console.WriteLine("Nome:");
+                nome = Console.ReadLine();
+                Console.WriteLine("Quantidade em estoque:");
+                qtdEstoque = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Valor de compra:");
+                valorCompra = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Valor de venda:");
+                valorVenda = Convert.ToDouble(Console.ReadLine());  
             }
         }
     }
