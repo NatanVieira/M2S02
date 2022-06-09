ca822ca42eb74bc5b8eb214934ed4f1506693714
Natanael Vieira <natanaelvieira130@gmail.com>
2022-06-09 11:34:17 -0300

    M2S02 - Exercicio 09

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 using System;
+using System.Globalization;
 namespace GeraEstoque {
 
     class Program {
@@ -8,8 +9,8 @@ namespace GeraEstoque {
         {
             string? nome = "";
             double? qtdEstoque = 0;
-            double? valorCompra = 0;
-            double? valorVenda = 0;
+            decimal? valorCompra = 0M;
+            decimal? valorVenda = 0M;
 
             System.Console.WriteLine("Seja Bem Vindo ao GeraEstoque 1.0");
             System.Console.WriteLine("------------------------------------------");
@@ -38,9 +39,15 @@ namespace GeraEstoque {
                 Console.WriteLine("Quantidade em estoque:");
                 qtdEstoque = Convert.ToDouble(Console.ReadLine());
                 Console.WriteLine("Valor de compra:");
-                valorCompra = Convert.ToDouble(Console.ReadLine());
+                valorCompra = Convert.ToDecimal(Console.ReadLine());
                 Console.WriteLine("Valor de venda:");
-                valorVenda = Convert.ToDouble(Console.ReadLine());  
+                valorVenda = Convert.ToDecimal(Console.ReadLine());
+                string id = Guid.NewGuid().ToString();
+                Console.WriteLine("ID: {0}", id);
+                Console.WriteLine("Nome: {0}", nome);
+                Console.WriteLine("Qtd: {0}", qtdEstoque);
+                Console.WriteLine($"R$ Compra: {valorCompra.ToString()}");
+                Console.WriteLine($"R$ Venda: {valorVenda}");
             }
         }
     }
